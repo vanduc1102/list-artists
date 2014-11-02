4be88b81893c539c2f3b550703495737166c7595
Duc Nguyen <vanducld@gmail.com>
2014-11-02 19:56:36 +0700

    add detail route

--- app/scripts/controllers/detail-controller.js
+++ app/scripts/controllers/detail-controller.js
@@ -7,6 +7,9 @@
  * # MainCtrl
  * Controller of the listArtistsApp
  */
-app.controller('DetailController', ['$scope',function ($scope) {
- 
+app.controller('DetailController', ['$scope','$http','$routeParams',function ($scope,$http,$routeParams) {
+	$http.get('scripts/controllers/data.json').success(function(data) {
+		$scope.artists = data;
+		$scope.whichItem= $routeParams.itemId;
+	});
 }]);
